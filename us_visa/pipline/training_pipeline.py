17668761a84f253c9d8473695e63e5df998fe75e
shaheennabi <ishaheennabi333@gmail.com>
2024-10-04 05:19:07 -0700

    updated

--- us_visa/pipline/training_pipeline.py
+++ us_visa/pipline/training_pipeline.py
@@ -3,25 +3,15 @@ from us_visa.exception import USvisaException
 from us_visa.logger import logging
 from us_visa.components.data_ingestion import DataIngestion
 from us_visa.components.data_validation import DataValidation
-from us_visa.components.data_transformation import DataTransformation
-from us_visa.components.model_trainer import ModelTrainer
-from us_visa.components.model_evaluation import ModelEvaluation
-from us_visa.components.model_pusher import ModelPusher
 
 
 from us_visa.entity.config_entity import (DataIngestionConfig,
-                                         DataValidationConfig,
-                                         DataTransformationConfig,
-                                         ModelTrainerConfig,
-                                         ModelEvaluationConfig,
-                                         ModelPusherConfig)
+                                         DataValidationConfig,)
+                                
 
 from us_visa.entity.artifact_entity import (DataIngestionArtifact,
                                             DataValidationArtifact,
-                                            DataTransformationArtifact,
-                                            ModelTrainerArtifact,
-                                            ModelEvaluationArtifact,
-                                            ModelPusherArtifact)
+)
 
 
 class TrainPipeline:
