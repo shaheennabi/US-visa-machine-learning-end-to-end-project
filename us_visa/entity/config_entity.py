3ff748f6f34268b53e20f45fd045ec97a3cd4886
shaheennabi <ishaheennabi333@gmail.com>
2024-10-05 03:04:11 -0700

    updated lot of files in data eval and pusher

--- us_visa/entity/config_entity.py
+++ us_visa/entity/config_entity.py
@@ -60,3 +60,15 @@ class ModelTrainerConfig:
     model_config_file_path: str = MODEL_TRAINER_MODEL_CONFIG_FILE_PATH
 
 
+@dataclass
+class ModelEvaluationConfig:
+    changed_threshold_score: float = MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE
+    bucket_name: str = MODEL_BUCKET_NAME
+    s3_model_key_path: str = MODEL_FILE_NAME
+
+
+
+@dataclass
+class ModelPusherConfig:
+    bucket_name: str = MODEL_BUCKET_NAME
+    s3_model_key_path: str = MODEL_FILE_NAME
